7d51d7d14132863460b1918e3699d230e7895e09
soulBenMJ <b.souliere@orange.fr>
2024-09-20 16:53:08 +0200

    Initialisation du projet
    Création du squelette et ajout du premier controller et de la première entité

---
+++ AP1-Karting/src/Entity/Utilisateur.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $login = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $motDePasse = null;
+
+    #[ORM\Column]
+    private ?bool $permission = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): static
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->motDePasse;
+    }
+
+    public function setMotDePasse(string $motDePasse): static
+    {
+        $this->motDePasse = $motDePasse;
+
+        return $this;
+    }
+
+    public function isPermission(): ?bool
+    {
+        return $this->permission;
+    }
+
+    public function setPermission(bool $permission): static
+    {
+        $this->permission = $permission;
+
+        return $this;
+    }
+}
