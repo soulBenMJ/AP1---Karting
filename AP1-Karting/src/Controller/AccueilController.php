d8840702844f5025823bbf85ef7b828e9d1586bc
soulBenMJ <b.souliere@orange.fr>
2024-10-08 11:05:12 +0200

    <Feat>(page_accueil):developpement fonctionnel de la page d'accueil
    ajout de la liste des horaires et de l'image de fond du titre

--- AP1-Karting/src/Controller/AccueilController.php
+++ AP1-Karting/src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OuvertureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(OuvertureRepository $ouvtr): Response
     {
+        $horaires = $ouvtr->findAll();
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'horaires' => $horaires,
         ]);
     }
 }
