e1898ad248078a3c897cc511650d1b9ab93f6f0d
soulBenMJ <b.souliere@orange.fr>
2024-10-18 08:59:45 +0200

    <hotfix>(modification): page acceuil
    la modification des horaires sont maintant reservé aux administarateurs

--- AP1-Karting/migrations/Version20241018065014.php
+++ AP1-Karting/migrations/Version20241018065014.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241017080102 extends AbstractMigration
+final class Version20241018065014 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241017080102 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE prestation (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(50) NOT NULL, prix_a INT NOT NULL, prix_e INT NOT NULL, temps VARCHAR(5) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, num_tel VARCHAR(255) NOT NULL, motif LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE prestation');
+        $this->addSql('DROP TABLE contact');
     }
 }
